fc9d472aa167bb94875aee465aed606bccb1e997
ABHAY-22 <51116785+ABHAY-22@users.noreply.github.com>
2024-04-08 02:13:49 +0530

    ak

---
+++ Day1/App.js
@@ -0,0 +1,56 @@
+// const heading=React.createElement("h1",{id:"abhay"},"I am from React world")
+// const root=ReactDOM.createRoot(document.getElementById("root"))
+
+
+// console.log(heading);//object
+// console.log(root)
+// root.render(heading)
+
+
+
+/*
+<div id ="parent">
+<div id ="child1">
+<h1></h1>==sibling
+<h2></h2>
+</div>
+
+<div id ="child1">
+<h1></h1>==sibling
+<h2></h2>
+</div>
+
+</div>
+
+
+
+*/
+
+const parent =React.createElement("div",{id:"parent"},
+[React.createElement("div",{id:"child"},
+[React.createElement("h1",{id:"heading"},"I am abhay from tundla"),
+React.createElement("h2",{id:"head"},"I am abhay from Noida")
+]),
+
+React.createElement("div",{id:"child2"},
+[React.createElement("h1",{id:"headi"},"I am abhay from tundla"),
+React.createElement("h2",{id:"hea"},"I am abhay from Noida")
+])]
+
+)
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
+
+console.log(parent)
+
+//JSX
+
+
+
+
+
+
